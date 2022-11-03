70a09757e7d3708d51830b1ec7dadc66f3658d40
Christopher Fauconnier <christopherfauconnier@gmail.com>
2022-11-03 12:26:22 +0100

    MVP CLI update
    lignes de commandes fonctionnelles pour le MVP

--- MVP.py
+++ MVP.py
@@ -1,7 +1,13 @@
-import random
+import random, cmd, sys
 class Credentials:
-    def __init__(self, dict={}):
+    def __init__(self, dict=None):
+        if dict is None:
+            dict = {}
         self.__dict = dict
+
+    def update(self, dict):
+        if dict:
+            self.__dict = dict
 
     def display_pwd(self):
         dict_pwd = self.__dict
@@ -26,17 +32,38 @@ class Credentials:
             sentence = sentence + site + "\n"
         return sentence
 
-dict_cred = {
-    "gmail": {"user@gmail.com": "test_mdp_gmail"},
-    "Facebook": {"user_facebook": "test_mdp_facebook"},
-    "twitter": {
-        "user_twitter": "test_mdp_twitter", 
-        "toto": "test_deuxieme_mdp"
-    }
-}
+class Interaction(cmd.Cmd):
+    intro = 'Bienvenue dans le gestionnaire de MDP. Tapez ? pour la liste des commandes.\n'
+    prompt = "(MVP) "
+
+    def do_init(self, line):
+        """Donne des valeurs tests par défaut"""
+        dict_cred = {
+            "gmail": {"user@gmail.com": "test_mdp_gmail"},
+            "Facebook": {"user_facebook": "test_mdp_facebook"},
+            "twitter": {
+                "user_twitter": "test_mdp_twitter", 
+                "toto": "test_deuxieme_mdp"
+            }
+        }
+        MVP.update(dict_cred)
+
+    def do_create(self, site="", username="", password=""):
+        """Permet de créer un nouveau MDP
+        format: site pseudo mdp"""
+        MVP.add_logs(site, username, password)
+    
+    def do_display(self, line):
+        """Affiche la liste des MDP"""
+        MVP.display_pwd()
+
+    def do_EXIT(self, line):
+        """Ferme le logiciel"""
+        return True
 
 if __name__ == "__main__":
-    MVP = Credentials(dict_cred)
-    MVP.display_pwd()
-    MVP.add_logs("twitter","test fonction", "motdepasse")
-    MVP.display_pwd()
+    MVP = Credentials()
+    Interaction().cmdloop()
+    # MVP.display_pwd()
+    # MVP.add_logs("twitter","test fonction", "motdepasse")
+    # MVP.display_pwd()
