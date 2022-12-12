71bf5cb66bbc96ed206a785c23722a01dfbb4f91
DelimeThibault <t.delime@students.ephec.be>
2022-12-12 16:14:12 +0100

    Revert "Correction Pylint et Path pour MacOS"
    This reverts commit 03f63d371e16c5dc7771b1d6c22588eba2ed5d4a.

--- Classes/interface/gui.py
+++ Classes/interface/gui.py
@@ -1,46 +1,44 @@
-"""Interface graphique Utilisateur"""
 import tkinter as tk
 import json
 from tkinter import filedialog as fd
-from pathlib import Path
 
 
 class Application(tk.Tk):
-    """Classes qui créée l'interface de l'application"""
     def __init__(self):
         tk.Tk.__init__(self)
-        self.__this_width = 600
-        self.__this_height = 400
+        self.__thisWidth = 600
+        self.__thisHeight = 400
 
         # Set icon
         try:
-            self.iconbitmap(Path(r"Classes\interface\lock.ico"))
-        except FileNotFoundError:
-            print('Fichier introuvable.')
-        except IOError:
-            print('Erreur IO.')
+            self.iconbitmap("Classes\interface\lock.ico")
+        except:
+            pass
+
         # Set title
         self.title("Gestionnaire de mots de passe")
 
         # Center the window
-        screen_width = self.winfo_screenwidth()
-        screen_height = self.winfo_screenheight()
+        screenWidth = self.winfo_screenwidth()
+        screenHeight = self.winfo_screenheight()
 
         # For left-align
-        left = (screen_width / 2) - (self.__this_width / 2)
+        left = (screenWidth / 2) - (self.__thisWidth / 2)
 
         # For right-align
-        top = (screen_height / 2) - (self.__this_height / 2)
+        top = (screenHeight / 2) - (self.__thisHeight / 2)
 
         # For top and bottom
-        self.geometry(f'{self.__this_width}x{self.__this_height}+{int(left)}+{int(top)}')
+        self.geometry('%dx%d+%d+%d' % (self.__thisWidth,
+                                       self.__thisHeight,
+                                       left, top))
 
         # Open default file location
         try:
-            with open(Path(r"db/credentials.json"), encoding="utf-8") as file:
-                data = json.load(file)
-                print(data)
-                self.creer_widgets()
+            file = open("db\credentials.json")
+            data = json.load(file)
+            file.close()
+            self.creer_widgets()
         except FileNotFoundError:
             self.label = tk.Label(self, text='Pas de fichier trouvé')
             self.label.pack()
@@ -55,20 +53,19 @@ class Application(tk.Tk):
             self.open_button.pack(expand=True)
 
     def creer_widgets(self):
-        """Création du widget qui affiche le texte et le bouton pour quitter"""
+
         self.label = tk.Label(self, text="J'adore Python !")
         self.bouton = tk.Button(self, text="Quitter", command=self.quit)
         self.label.pack()
         self.bouton.pack()
 
     def select_file(self):
-        """Permet de sélectionner un fichier si il n'a pas été trouvé"""
         filetypes = (
             ('json files', '*.json'),
             ('All files', '*.*')
         )
 
-        fd.askopenfilename(
+        filename = fd.askopenfilename(
             title='Open a file',
             initialdir='/',
             filetypes=filetypes)
